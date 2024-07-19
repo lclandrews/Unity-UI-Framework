572d70097c8bf9c0f468cde6c66fe49459ddb5ef
Luke Andrews <lclandrews@gmail.com>
2024-07-19 13:02:02 +0100

    Additional extension method to create a playable from within an window

--- Packages/com.lukeandrews.ui-framework/Runtime/Animation/AnimationExtensions.cs
+++ Packages/com.lukeandrews.ui-framework/Runtime/Animation/AnimationExtensions.cs
@@ -20,5 +20,14 @@ namespace UIFramework
             Animation animation = window.CreateAnimation(type, length);
             return new AnimationPlayable(animation, startTime, playMode, easingMode, timeMode);
         }
+
+        public static AnimationPlayable CreatePlayable(this IWindow window, GenericWindowAnimationType type, float length, AccessOperation accessOperation,
+            float startOffset = 0.0F, EasingMode easingMode = EasingMode.Linear, TimeMode timeMode = TimeMode.Scaled)
+        {
+            PlayMode playMode = accessOperation == AccessOperation.Open ? PlayMode.Forward : PlayMode.Reverse;
+            float startTime = playMode == PlayMode.Forward ? startOffset : length - startOffset;
+            Animation animation = window.CreateAnimation(type, length);
+            return new AnimationPlayable(animation, startTime, playMode, easingMode, timeMode);
+        }
     }
 }
