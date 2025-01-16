abf7efc4706ffb7215d2202ad980594022e08f72
Luke Andrews <lclandrews@gmail.com>
2025-01-16 12:46:22 +0000

    Minor update to controller initialisation flow

--- Packages/com.lukeandrews.ui-framework/Runtime/Controller.cs
+++ Packages/com.lukeandrews.ui-framework/Runtime/Controller.cs
@@ -166,9 +166,9 @@ namespace UIFramework
             _navigation.OnNavigationUpdate += OnNavigationUpdate;
 
             _transitionManager = new WindowTransitionManager(TimeMode);
-
-            SetBackButtonActive(false);
+            
             OnInit();
+            SetBackButtonActive(false);
 
             AccessState = AccessState.Closed;
         }
